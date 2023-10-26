1f1f3d0040445075a13ed12d88d85e7c0aec15c6
POLVO <polvorine.dev@gmail.com>
2023-10-26 03:17:11 -0700

    sala

--- Pruebas/Pruebas2.py
+++ Pruebas/Pruebas2.py
@@ -1,12 +1,10 @@
 import re
-#cambio
+
 # PLANTILLAS
 v1 = 1
 v2 = 2
 # MOVER
 mov = f"mov {v1} {v2}\n"
-v2 = 5
-print(mov)
 # OPERACIONES ARITMETICAS
 add = f"add {v1} {v2}\n"
 sub = f"sub {v1} {v2}\n"
@@ -14,26 +12,30 @@ mul = f"mul {v1} {v2}\n"
 div = f"div {v1} {v2}\n"
 # GUARDAR
 sto = f"sto {v1} {v2}\n"
-# SALTOS
+# declaraciones
 dint = f"{v1} dw  {v2}\n"
 dfloat = f"{v1} dd  {v2}\n"
 dstring = f"{v1} dw  {v2}\n"
 dexpresion = f"{v1} dd 0;\n"
+deprint = f"text {v1} db {v2},0\n"
 # SECCIONES
 secdata = "section.data" + "\n"
-secmain = "section.main" + "\n"
+sectext = "section.main" + "\n"
+globalmain = "global main " + "\n"
+main = "main:" + "\n"
 # ENSAMBLADOR ES EL STRING QUE GENERAMOS
 ensamblador = ""
 # TIPOS DE DECLARACIONES Y INSTRUCCIONES
 entero = r'\b(\w+)\s+(\w+)\s*=\s*(-?\d+)\b'
 cadena = r'\b(\w+)\s+(\w+)\s*=\s*\'([^\']*)\'|\"([^\"]*)\"'
-imprimir = r'print\s*\(\s*([\w.+\-*/]+|\'[^\']*\'|"[^"]*"\s*)\)'
+imprimir = r'print\s*\(\s*([\'"][^\'"]+[\'"])\s*\)'
+imprimirvariables = r'print\s*\(\s*([a-zA-Z_]\w*)\s*\)'
 expresion = r'\b(\w+)\s+(\w+)\s*=\s*([\w.+\-*/]+)'
 # GUARDAMOS LINEAS DE CODIGO
 lineasdecodigo = []
 # LA TAGLA DE SIMBOLOS
 symbol_table = []
-# un contador
+# contador
 contador = 1
 # CODIGO FUENTE
 programa = """
@@ -76,24 +78,35 @@ for line in lines:
     # se encontro una cadena
     elif re.match(cadena, line):
         match = re.match(cadena, line)
-        var_type = match.group(1)
-        var_id = match.group(2)
         var_value = str(match.group(3))
-        print(contador)
+        v1 = contador
+        v2 = var_value
+        ensamblador += str(contador)
+        deprint = f"text{v1} db {v2},0\n"
+        ensamblador += deprint
         contador = contador + 1
 
     # se encontro la instruccion imprimir
     elif re.match(imprimir, line):
-        texto = "mensaje"
-        print(contador)
+        match = re.match(imprimir, line)
+        texto = match.group(1)
+        v1 = contador
+        v2 = texto
+        ensamblador += str(contador)
+        deprint = f"text{v1} db {v2},0\n"
+        ensamblador += deprint
         contador = contador + 1
 
+    elif re.match(imprimirvariables, line):
+        print('')
 # encontramos operadores y operandos
-# operator_pattern = re.compile(r'[+\-*/]')
-# operand_pattern = re.compile(r'\b(\w+|\d+)\b')
+operadores = re.compile(r'[+\-*/]')
+operandos = re.compile(r'\b(\w+|\d+)\b')
 # creamos donde guardarlos
-# operadores_encontrados = []
-# operandos_encontrados = []
+operadores_encontrados = []
+operandos_encontrados = []
+ensamblador += globalmain
+ensamblador += main
 for line in lines:
     # solo para evitar errores
     if re.match(entero, line):
@@ -103,6 +116,9 @@ for line in lines:
         ensamblador += ";imprimir un mensaje" + "\n"
     # analisis y impresion de  expresiones
     elif re.match(expresion, line):
+
         ensamblador += ";analizar una expresion" + "\n"
+    elif re.match(imprimirvariables, line):
+        ensamblador += ";imprimir un mensaje" + "\n"
 
 print(ensamblador)
